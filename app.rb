a7641ef5c4c0c6c57216f338b4f3b50043e4d808
takuminish <s15t253@stu.kagawa-u.ac.jp>
2018-07-05 01:29:20 +0900

    first commit

---
+++ app.rb
@@ -0,0 +1,71 @@
+require 'sinatra'
+
+get '/room' do
+
+  response = {
+    code: "room"
+  }
+
+  response.to_json
+
+end
+
+get '/aircon' do
+
+  response = {
+    code: "aircon"
+  }
+
+  response.to_json
+
+end
+
+get '/cool' do
+
+  response = {
+    code: "cool"
+  }
+
+  response.to_json
+
+end
+
+get '/hot' do
+
+  response = {
+    code: "hot"
+  }
+
+  response.to_json
+
+end
+
+get '/off' do
+
+  response = {
+    code: "off"
+  }
+
+  response.to_json
+
+end
+
+get '/up' do
+
+  response = {
+    code: "up"
+  }
+
+  response.to_json
+
+end
+
+get '/down' do
+
+  response = {
+    code: "down"
+  }
+
+  response.to_json
+
+end
