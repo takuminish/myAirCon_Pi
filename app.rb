f73f46dbb7020d951898424fc227df7cf4110a60
takuminish <s15t253@stu.kagawa-u.ac.jp>
2018-09-19 23:53:56 +0900

    check_request remove

--- app.rb
+++ app.rb
@@ -1,107 +1,79 @@
 require 'sinatra'
 require 'dotenv'
 
-def request_check 
-  p  request.env["HTTP_X_REQUESTED_WITH"]
-  p ENV["X"]
-  if request.env["HTTP_X_REQUESTED_WITH"] == ENV["X"]
-    return true
-  else
-    return false
-  end
-
-end
-
-
 
 Dotenv.load
 
 get '/room' do
 
-  if request_check
-    response = {
-      code: "room"
-    }
 
-    response.to_json
-
-  end
+  response = {
+    code: "room"
+  }
+  response.to_json
   
 end
 
 get '/aircon' do
 
-  if request_check
-    response = {
-      code: "aircon"
-    }
 
-    response.to_json
-  end
+  response = {
+    code: "aircon"
+  }
+
+  response.to_json
   
 end
 
 get '/cool' do
 
-  if request_check
-    response = {
-      code: "cool"
-    }
 
-    system("sh aircon.sh cool")        
-    response.to_json
+  response = {
+    code: "cool"
+  }
 
-  end
-
+  system("sh aircon.sh cool")
+  response.to_json
 end
 
 get '/hot' do
 
-  if request_check
     response = {
-      code: "hot"
-    }
+    code: "hot"
+  }
 
-    system("sh aircon.sh hot")    
-    response.to_json
-  end
-  
+  system("sh aircon.sh hot")
+  response.to_json
 end
 
 get '/off' do
+response = {
+    code: "off"
+  }
 
-  if request_check
-    response = {
-      code: "off"
-    }
-    system("sh aircon.sh off")    
-    response.to_json
-  end
-  
+  system("sh aircon.sh off")
+  response.to_json
+    
 end
 
 get '/up' do
 
-  if request_check
-    response = {
-      code: "up"
-    }
+  response = {
+    code: "up"
+  }
 
-    response.to_json
-  end
+  response.to_json
  
 end
 
 get '/down' do
 
-  if request_check
-    response = {
-      code: "down"
-    }
+  response = {
+    code: "down"
+  }
 
-    response.to_json
-  end
- 
+  response.to_json
+  
 end
 
                                                          
